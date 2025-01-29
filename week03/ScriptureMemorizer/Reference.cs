1f2345e3313b33d0b9c04448bff3fbc88ba2b85b
gideonagbavor8 <gagbavor@byupathway.edu>
2025-01-29 12:19:06 +0000

    W04 Assignment: YouTube Video Program

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -2,10 +2,10 @@ using System;
 
 class Reference
 {
-    private string _book { get; }
-    private int _chapter { get; }
-    private int _verse { get; }
-    private int? _endVerse { get; }
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int? _endVerse;
 
     public Reference(string book, int chapter, int verse, int? endVerse = null)
     {
@@ -15,8 +15,76 @@ class Reference
         _endVerse = endVerse;
     }
 
+    public string GetBook()
+    {
+        return _book;
+    }
+
+    public int GetChapter()
+    {
+        return _chapter;
+    }
+
+    public int GetVerse()
+    {
+        return _verse;
+    }
+
+    public int? GetEndVerse()
+    {
+        return _endVerse;
+    }
+
     public string GetDisplayText()
     {
         return _endVerse.HasValue ? $"{_book} {_chapter}:{_verse}-{_endVerse}" : $"{_book} {_chapter}:{_verse}";
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// using System;
+
+// class Reference
+// {
+//     private string _book { get; }
+//     private int _chapter { get; }
+//     private int _verse { get; }
+//     private int? _endVerse { get; }
+
+//     public Reference(string book, int chapter, int verse, int? endVerse = null)
+//     {
+//         _book = book;
+//         _chapter = chapter;
+//         _verse = verse;
+//         _endVerse = endVerse;
+//     }
+
+//     public string GetDisplayText()
+//     {
+//         return _endVerse.HasValue ? $"{_book} {_chapter}:{_verse}-{_endVerse}" : $"{_book} {_chapter}:{_verse}";
+//     }
+// }
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,106 @@
 using System;
+using System.Collections.Generic;
+
+class Comment
+{
+    private string commenterName;
+    private string text;
+
+    public Comment(string commenterName, string text)
+    {
+        this.commenterName = commenterName;
+        this.text = text;
+    }
+
+    public string GetCommenterName()
+    {
+        return commenterName;
+    }
+
+    public string GetText()
+    {
+        return text;
+    }
+}
+
+class Video
+{
+    private string title;
+    private string author;
+    private int lengthInSeconds;
+    private List<Comment> comments;
+
+    public Video(string title, string author, int lengthInSeconds)
+    {
+        this.title = title;
+        this.author = author;
+        this.lengthInSeconds = lengthInSeconds;
+        comments = new List<Comment>();
+    }
+
+    public void AddComment(Comment comment)
+    {
+        comments.Add(comment);
+    }
+
+    public int GetNumberOfComments()
+    {
+        return comments.Count;
+    }
+
+    public void DisplayComments()
+    {
+        foreach (var comment in comments)
+        {
+            Console.WriteLine($"- {comment.GetCommenterName()}: {comment.GetText()}");
+        }
+    }
+
+    public string GetVideoDetails()
+    {
+        return $"Title: {title}\nAuthor: {author}\nLength: {lengthInSeconds} seconds\nNumber of Comments: {GetNumberOfComments()}";
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        // Create videos
+        Video video1 = new Video("Understanding Abstraction", "Gideon J.", 300);
+        video1.AddComment(new Comment("Peace", "Great explanation!"));
+        video1.AddComment(new Comment("Millicent", "Very helpful, thanks!"));
+        video1.AddComment(new Comment("Elinam", "I finally understand abstraction."));
+        video1.AddComment(new Comment("Peter", "Clear and concise."));
+
+        Video video2 = new Video("Introduction to Classes", "Agbotse Harrison", 450);
+        video2.AddComment(new Comment("Elisha", "Nice video!"));
+        video2.AddComment(new Comment("Enam", "Could you explain more about constructors?"));
+        video2.AddComment(new Comment("Amenu", "Very informative."));
+        video2.AddComment(new Comment("Sarah", "Helped me a lot, thanks!"));
+
+        Video video3 = new Video("Advanced Inheritance", "John Doe", 600);
+        video3.AddComment(new Comment("Nancy", "Very informative!"));
+        video3.AddComment(new Comment("Almira", "I learned a lot, thanks!"));
+        video3.AddComment(new Comment("Afeke", "Great examples."));
+        video3.AddComment(new Comment("Godsway", "Well explained."));
+
+        Video video4 = new Video("Polymorphism Explained", "Gideon Agbavor", 720);
+        video4.AddComment(new Comment("Gideon", "Great examples!"));
+        video4.AddComment(new Comment("Christabel", "Could you do more videos on this topic?"));
+        video4.AddComment(new Comment("Benedict", "Very helpful."));
+        video4.AddComment(new Comment("Fafali", "Excellent explanation."));
+
+        // Store videos in a list
+        List<Video> videos = new List<Video> { video1, video2, video3, video4 };
+
+        // Display video information
+        foreach (var video in videos)
+        {
+            Console.WriteLine(video.GetVideoDetails());
+            Console.WriteLine("Comments:");
+            video.DisplayComments();
+            Console.WriteLine();
+        }
     }
 }
